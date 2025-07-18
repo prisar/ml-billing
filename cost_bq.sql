ca3161863dcb6ecb968f29115c9db89d786d45a3
pritimay <prisar230@gmail.com>
2025-07-18 10:49:28 +0530

    Update cost_bq.sql

--- cost_bq.sql
+++ cost_bq.sql
@@ -10,3 +10,18 @@ WHERE
 GROUP BY team
 ORDER BY total_cost DESC
 
+
+
+-- cost 
+
+SELECT
+-- DISTINCT labels
+  (SELECT value FROM UNNEST(labels) WHERE key = "team") AS team,
+  SUM(cost) AS total_cost
+FROM
+  `mrc-quant-ml.all_billing_data.gcp_billing_export_resource_v1_*`
+WHERE
+  service.description = "Vertex AI"
+  -- AND resource.name LIKE "%gemini%"
+GROUP BY team
+ORDER BY total_cost DESC
